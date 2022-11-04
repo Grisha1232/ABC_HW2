a4acc8aff59c6a3dafc4423f72d95d3af2789235
Grisha1232 <58052549+Grisha1232@users.noreply.github.com>
2022-11-04 23:22:23 +0300

    Delete inputConsole.c

--- C code/inputConsole.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void inputFromConsole(char **string, size_t *length) {
-    size_t inputLen = 0;
-    printf("Type string: ");
-    *length = getline(&*string, &inputLen, stdin);
-}
