147a4aaa290dbe6ec460cca04c1912c138f0d88a
Grisha1232 <58052549+Grisha1232@users.noreply.github.com>
2022-11-04 22:51:48 +0300

    Create inputFile.c

---
+++ C code/inputFile.c
@@ -0,0 +1,13 @@
+int inputFromFile(char* in) {
+    FILE* input = fopen(in, "r");
+    char c;
+    int count = 0;
+    while(!feof(input)) {
+        c = fgetc(input);
+        if (isPunctuationMark(c)) {
+            count++;
+        }
+    }
+    fclose(input);
+    return count;
+}
