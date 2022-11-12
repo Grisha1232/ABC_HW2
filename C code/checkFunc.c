20488d81f17e1a472c119c318973d33e5d8f165f
Grisha1232 <58052549+Grisha1232@users.noreply.github.com>
2022-11-12 13:52:13 +0300

    Update checkFunc.c

--- C code/checkFunc.c
+++ C code/checkFunc.c
@@ -1,30 +1,52 @@
 int isPunctuationMark(const char c) {
     switch (c) {
         case 33:        // !
-        case 34:        // "
-        case 39:        // '
-        case 40:        // (
-        case 41:        // )
-        case 44:        // ,
-        case 45:        // -
-        case 46:        // .
-        case 47:        // /
-        case 58:        // :
-        case 59:        // ;
-        case 60:        // <
-        case 62:        // >
-        case 63:        // ?
-        case 91:        // [
-        case 92:        // backslash
-        case 93:        // ]
-        case 94:        // ^
-        case 95:        // _
-        case 96:        // `
-        case 123:       // {
-        case 124:       // |
-        case 125:       // }
-            return 1;
-        default:
             return 0;
+        case 34:        // "
+            return 1;
+        case 39:        // '
+            return 2;
+        case 40:        // (
+            return 3;
+        case 41:        // )
+            return 4;
+        case 44:        // ,
+            return 5;
+        case 45:        // -
+            return 6;
+        case 46:        // .
+            return 7;
+        case 47:        // /
+            return 8;
+        case 58:        // :
+            return 9;
+        case 59:        // ;
+            return 10;
+        case 60:        // <
+            return 11;
+        case 62:        // >
+            return 12;
+        case 63:        // ?
+            return 13;
+        case 91:        // [
+            return 14;
+        case 92:        // backslash
+            return 15;
+        case 93:        // ]
+            return 16;
+        case 94:        // ^
+            return 17;
+        case 95:        // _
+            return 18;
+        case 96:        // `
+            return 19;
+        case 123:       // {
+            return 20;
+        case 124:       // |
+            return 21;
+        case 125:       // }
+            return 22;
+        default:
+            return -1;
     }
 }
